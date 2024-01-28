a8b4da0362725e594d5dd8dfb59211d0597754d1
Yukine16 <93941711+Yukine16@users.noreply.github.com>
2024-01-28 21:07:40 +0000

    Control de errores

--- app/src/main/java/com/dam/videojuegos/screens/LoginScreen.kt
+++ app/src/main/java/com/dam/videojuegos/screens/LoginScreen.kt
@@ -2,12 +2,18 @@ package com.dam.videojuegos.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ErrorOutline
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -17,17 +23,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(navController: NavHostController, auth: FirebaseAuth) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var dialogoError by remember { mutableStateOf(false) }
+    var mensajeError by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -50,7 +59,10 @@ fun LoginScreen(navController: NavHostController, auth: FirebaseAuth) {
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = { iniciarSesion(navController, auth, email, password) },
+            onClick = { iniciarSesion(navController, auth, email, password) {error ->
+                mensajeError = error
+                dialogoError = true
+            }},
             modifier = Modifier.width(200.dp)
         ) {
             Text("Iniciar sesión")
@@ -63,19 +75,69 @@ fun LoginScreen(navController: NavHostController, auth: FirebaseAuth) {
             Text("Registrarse")
         }
     }
+
+    if (dialogoError) {
+        AlertDialog(
+            onDismissRequest = { dialogoError = false },
+            title = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Icon(imageVector = Icons.Outlined.ErrorOutline, contentDescription = null)
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text("Error de inicio de sesión", fontWeight = FontWeight.Bold)
+                }
+            },
+            text = {
+                Text(mensajeError)
+            },
+            confirmButton = {
+                Button(
+                    onClick = { dialogoError = false },
+                    modifier = Modifier.width(100.dp)
+                ) {
+                    Text("OK")
+                }
+            }
+        )
+    }
 }
 
 private fun iniciarSesion(
     navController: NavHostController,
     auth: FirebaseAuth,
     email: String,
-    password: String
+    password: String,
+    onError: (String) -> Unit
 ) {
-    auth.signInWithEmailAndPassword(email, password)
-        .addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                navController.navigate("main")
-            } else {
+    when {
+        email.isEmpty() || password.isEmpty() -> {
+            onError("Debe rellenar todos los campos.")
+        }
+        !validarEmail(email) -> {
+            onError("El formato del correo electrónico no es válido.")
+        }else -> {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    navController.navigate("main")
+                } else {
+                    val exception = task.exception
+                    if(exception is FirebaseAuthInvalidCredentialsException){
+                        onError("Credenciales incorrectas.")
+                    }else {
+                        val errorMessage = exception?.message ?: "Error"
+                        onError(errorMessage)
+                    }
+
+                }
             }
         }
+    }
+
+}
+
+private fun validarEmail(email: String): Boolean {
+    return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 }
--- app/src/main/java/com/dam/videojuegos/screens/RegisterScreen.kt
+++ app/src/main/java/com/dam/videojuegos/screens/RegisterScreen.kt
@@ -2,12 +2,18 @@ package com.dam.videojuegos.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ErrorOutline
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -17,10 +23,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseAuth
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -28,6 +34,8 @@ import com.google.firebase.auth.FirebaseAuth
 fun RegisterScreen(navController: NavHostController, auth: FirebaseAuth) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var dialogoError by remember { mutableStateOf(false) }
+    var mensajeError by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -50,7 +58,10 @@ fun RegisterScreen(navController: NavHostController, auth: FirebaseAuth) {
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = { registrarse(navController, auth, email, password) },
+            onClick = { registrarse(navController, auth, email, password){error ->
+                mensajeError = error
+                dialogoError = true
+            } },
             modifier = Modifier.width(200.dp)
         ) {
             Text("Registrarse")
@@ -64,19 +75,67 @@ fun RegisterScreen(navController: NavHostController, auth: FirebaseAuth) {
         }
 
     }
+
+    if (dialogoError) {
+        AlertDialog(
+            onDismissRequest = { dialogoError = false },
+            title = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Icon(imageVector = Icons.Outlined.ErrorOutline, contentDescription = null)
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text("Error de registro", fontWeight = FontWeight.Bold)
+                }
+            },
+            text = {
+                Text(mensajeError)
+            },
+            confirmButton = {
+                Button(
+                    onClick = { dialogoError = false },
+                    modifier = Modifier.width(100.dp)
+                ) {
+                    Text("OK")
+                }
+            }
+        )
+    }
 }
 
 private fun registrarse(
     navController: NavHostController,
     auth: FirebaseAuth,
     email: String,
-    password: String
+    password: String,
+    onError: (String) -> Unit
 ) {
-    auth.createUserWithEmailAndPassword(email, password)
-        .addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                navController.navigate("login")
-            } else {
+    when {
+        email.isEmpty() || password.isEmpty() -> {
+            onError("Debe rellenar todos los campos")
+        }
+        !validarEmail(email) -> {
+            onError("El formato del correo electrónico no es válido")
+        }
+        password.length < 6 -> {
+            onError("La contraseña debe tener al menos 6 caracteres.")
+        }else -> {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    navController.navigate("login")
+                } else {
+                    val exception = task.exception
+                    val errorMessage = exception?.message ?: "Error"
+                    onError(errorMessage)
+                }
             }
         }
+    }
+
+}
+
+private fun validarEmail(email: String): Boolean {
+    return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 }
